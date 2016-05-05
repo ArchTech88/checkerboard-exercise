86a7bfec08b0a8189dab6f0460bcaee619d7bef2
Alex Cheng <alexcheng@Alexs-MacBook-Pro.local>
2016-05-04 17:46:12 -0700

    gradient specialized

--- script.js
+++ script.js
@@ -11,32 +11,21 @@ function getRandomColor() {
     return color;
 }
 
-var body = document.getElementsByTagName('body')[0]; //random colored
 
-  for (var i = 0; i < 81; i++) {
-    var tile = document.createElement('div');
 
-    tile.style.width = '11.1%';
-    tile.style.paddingBottom = '11.1%';
-    tile.style.float = 'left';
 
-    tile.style.backgroundColor = getRandomColor()
-    body.appendChild(tile);
-  }
+  var body = document.getElementsByTagName('body')[0]; //gradient
 
-  //
-  // var body = document.getElementsByTagName('body')[0]; //gradient
-  //
-  //   for (var i = 0; i < 81; i++) {
-  //     var tile = document.createElement('div');
-  //
-  //     tile.style.width = '11.1%';
-  //     tile.style.paddingBottom = '11.1%';
-  //     tile.style.float = 'left';
-  //     if (i % 2 === 0) {
-  //       tile.style.backgroundColor = 	'rgb(' +(255*(1 - i*.99)) + ',0,0)'
-  //     } else {
-  //       tile.style.backgroundColor = 'black';
-  //     }
-  //     body.appendChild(tile);
-  //   }
+    for (var i = 0; i < 81; i++) {
+      var tile = document.createElement('div');
+
+      tile.style.width = '11.1%';
+      tile.style.paddingBottom = '11.1%';
+      tile.style.float = 'left';
+      if (i % 2 === 0) {
+        tile.style.backgroundColor = 	'rgba(0,0,255, '+(1 -.01*i) + ')'
+      } else {
+        tile.style.backgroundColor = 'rgba(255,0,150, '+(1 -.01*i) + ')'
+      }
+      body.appendChild(tile);
+    }
