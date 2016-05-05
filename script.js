1f56f0be32c8f04b99d4f9c37c3c905575066f4f
Alex Cheng <alexcheng@Alexs-MacBook-Pro.local>
2016-05-04 17:27:20 -0700

    random colors

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 // Your JS goes here
 
-// var body = document.getElementsByTagName('body')[0]; //normal checkerboard
-//
-//     for (var i = 0; i < 81; i++) {
-//       var div = document.createElement('div');
-//
-//       div.style.width = '11.1%';
-//       div.style.paddingBottom = '11.1%';
-//       div.style.float = 'left';
-//
-//       if (i % 2 === 0) {
-//         div.style.backgroundColor = 'math';
-//       } else {
-//         div.style.backgroundColor = 'black';
-//       }
-//       body.appendChild(div);
-//     }
+
 
 function getRandomColor() {
     var hexLetters = '0123456789ABCDEF'.split('');
